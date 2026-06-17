cb7224142e15885f042a3624fc448443e427a95a
nthBit <207577365+nthBit@users.noreply.github.com>
2026-06-17 12:33:38 +0100

    Design base calculator frame

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(num1, sym, num2) {
+    if (sym === '+') {
+        return add(num1, num2);
+    } else if (sym === '-') {
+        return subtract(num1, num2);
+    } else if (sym === '*') {
+        return multiply(num1, num2);
+    } else if (sym === '/') {
+        return divide(num1, num2);
+    }
+}
